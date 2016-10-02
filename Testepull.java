93b537512f2e60b4d35f1be3b30a66f04951d89a
gittestes <vitorinfo@gmail.com>
2016-10-02 00:21:00 -0300

    Update Testepull.java

--- Testepull.java
+++ Testepull.java
@@ -4,10 +4,10 @@ public clas Testepull{
 
     	private String nome;
     	private int idade;
-<<<<<<< HEAD
     	private int numero;
-=======
->>>>>>> 8166ea6b0cf5b8f344b804e67f5dd0089bf7009e
+    }
     
+    public String teste(){
+        return "teste";
     }
 }
